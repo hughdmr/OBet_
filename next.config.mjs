e99161b24d00fa833212f56fbe897ca683daa875
Alexandre Gravereaux <alexandre.gravereaux@student-cs.fr>
2024-06-16 15:08:43 +0200

    update home screen with dashboard

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-};
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 export default function HomePage() {
   return (
   <div className="root">
-    Bienvenue sur la page principale de OptiBet (OB)
   </div>
 );
 }
